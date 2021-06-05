a5e5131dcd978b11b130dfc2bbb29642e51b42e9
Helvi Lainekallio <helvi.lainekallio@gmail.com>
2021-06-05 19:49:54 +0300

    frontend added to Heroku app

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('reqBody', (req, res) => {
 	return ""
 })
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'))
 
